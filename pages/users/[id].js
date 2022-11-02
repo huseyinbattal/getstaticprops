8b64825309d58b2ebd520f81f2dbba7aaa9afcaf
Hüseyin BATTAL <95706081+huseyinbattal@users.noreply.github.com>
2022-11-02 20:57:07 +0300

    add getServerSideProps function

---
+++ pages/users/[id].js
@@ -0,0 +1,46 @@
+import styles from "../../styles/Home.module.css";
+import Link from "next/link";
+
+export default function UserDetails({ user }) {
+  return (
+    <div className={styles.container}>
+      <table id="users">
+        <tr>
+          <th>Name</th>
+          <th>Email</th>
+          <th>Phone No</th>
+          <th>WebSite</th>
+        </tr>
+
+          <tr>
+            <td>{user.name}</td>
+            <td>{user.email}</td>
+            <td>{user.phone}</td>
+            <td>{user.website}</td>
+          </tr>
+
+          </table>
+          
+          <Link href="/">
+              Go Back
+          </Link>
+
+    </div>
+  );
+}
+
+export const getServerSideProps = async (context) => {
+
+  const res = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${context.params.id}`
+  );
+    
+    const user = await res.json();
+
+    return {
+        props: {
+            user
+        }
+    }
+    
+};
